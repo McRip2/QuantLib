6d81d9b45b824b8257496f400b0808500c061bfb
McRip2 <marc@gerritzen.name>
2022-08-17 08:52:09 +0000

    implement portfolio monte carlo

--- ql/methods/montecarlo/portfoliomontecarlo.hpp
+++ ql/methods/montecarlo/portfoliomontecarlo.hpp
@@ -9,33 +9,45 @@
 
 namespace QuantLib {
 
-    class PortfolioMonteCarlo {
-    public:
-        MonteCarlo() {}
+  class PortfolioMonteCarlo {
+  public:
+    PortfolioMonteCarlo() {}
 
-        Matrix simulate(const std::vector<ext::shared_ptr<Instrument>>& securities,
-                        std::vector<RelinkableHandle<Quote>>& handles,
-                        const Matrix& field_sims) {
+    Matrix simulate(const std::vector<ext::shared_ptr<Instrument>>& securities,
+                    std::vector<RelinkableHandle<Quote>>& handles,
+                    const Matrix& field_sims) {
 
-            Matrix simValues(field_sims.rows(), field_sims.columns());
+      assert (field_sims.columns() == handles.size());
+      Matrix simValues(field_sims.rows(), securities.size());
+      std::cout << "Beginning simulation n=" << field_sims.rows() << "m=" << field_sims.columns() << std::endl;
 
-            for(int t = 0; t < field_sims.rows(); t++)
-                {
-                    // update handles
-                    for(int i = 0; i < field_sims.columns(); i++)
-                        {
-                            boost::shared_ptr<SimpleQuote> quote(new SimpleQuote(field_sims[t][i]));
-                            handles[i].linkTo(quote);
-                        }
+      for(int t = 0; t < field_sims.rows(); t++)
+        {
+          // update handles
+          for(int i = 0; i < field_sims.columns(); i++)
+            {
+              boost::shared_ptr<SimpleQuote> quote(new SimpleQuote(field_sims[t][i]));
+              handles[i].linkTo(quote);
+            }
 
-                    // calc value of securities
-                    for(int j = 0; j < securities.size(); j++) {
-                        simValues[t][j] = securities[j]->NPV();
-                    }
-                }
-            return simValues;
+          // calc value of securities
+          for(int j = 0; j < securities.size(); j++) {
+            simValues[t][j] = securities[j]->NPV();
+          }
         }
-    };
+      return simValues;
+    }
+  };
+
+  // class TTTest {
+  // public:
+  //   void test(const std::vector<ext::shared_ptr<Instrument>>& securities) {
+
+  //     for(int i = 0; i < securities.size(); i++) {
+  //    std::cout << securities[i]->NPV() << std::endl;
+  //     }
+  //     return;
+  //   };
 }
 
 
