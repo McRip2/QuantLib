56bea9bcbd950646e4f79fe198237baee888844b
McRip2 <marc@gerritzen.name>
2022-08-18 16:23:34 +0000

    simplity

--- ql/methods/montecarlo/portfoliomontecarlo.hpp
+++ ql/methods/montecarlo/portfoliomontecarlo.hpp
@@ -15,26 +15,24 @@ namespace QuantLib {
 
     Matrix simulate(const std::vector<ext::shared_ptr<Instrument>>& securities,
                     std::vector<RelinkableHandle<Quote>>& handles,
-		    std::vector<RelinkableHandle<Quote>>& vol_shift_handles,
-                    const Matrix& field_sims,
-		    const Matrix& vol_shifts) {
+                    const Matrix& sims) {
 
-      assert (field_sims.columns() == handles.size());
-      assert (field_sims.columns() == vol_shifts.columns());
-      assert (field_sims.rows() == vol_shifts.rows());
+      assert (sims.columns() == handles.size());
       
-      Matrix simValues(field_sims.rows(), securities.size());
-      std::cout << "Beginning simulation n=" << field_sims.rows() << " " << "m=" << field_sims.columns() << std::endl;
+      Matrix simValues(sims.rows(), securities.size());
+      std::cout << "Beginning simulation n=" << sims.rows() << " " << "m=" << sims.columns() << std::endl;
 
-      for(int t = 0; t < field_sims.rows(); t++)
+      for(int t = 0; t < sims.rows(); t++)
         {
+	  if(t%100 == 0) {
+	    std::cout << "t=" << t << std::endl;
+	  }
+	  
           // update handles
-          for(int i = 0; i < field_sims.columns(); i++)
+          for(int i = 0; i < sims.columns(); i++)
             {
-              boost::shared_ptr<SimpleQuote> quote(new SimpleQuote(field_sims[t][i]));
-	      boost::shared_ptr<SimpleQuote> vol_shift(new SimpleQuote(vol_shifts[t][i]));
+              boost::shared_ptr<SimpleQuote> quote(new SimpleQuote(sims[t][i]));
               handles[i].linkTo(quote);
-	      vol_shift_handles[i].linkTo(vol_shift);
             }
 
           // calc value of securities
